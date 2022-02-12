1087954e3a66306fbd4b25fc1188e14d0397cbd3
beanflame <beanflame@qq.com>
2022-02-12 23:29:13 +0800

    Up

---
+++ src/main/java/io/github/ationmc/loader/launch/Tweaker.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (C) 2022 Enaium
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ationmc.loader.launch;
+
+import io.github.ationmc.loader.mod.ModInfo;
+import com.google.gson.Gson;
+import net.minecraft.launchwrapper.ITweaker;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraft.launchwrapper.LogWrapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.jar.JarFile;
+
+/**
+ * @author Enaium
+ */
+public class Tweaker implements ITweaker {
+
+    private final List<String> arguments = new ArrayList<>();
+
+    private String gameDir = System.getProperty("user.dir");
+
+    @Override
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
+        this.arguments.addAll(args);
+        if (gameDir != null) {
+            this.gameDir = gameDir.getAbsolutePath();
+        }
+
+        addArgument("--gameDir", gameDir != null ? gameDir.getAbsolutePath() : this.gameDir);
+        addArgument("--assetsDir", assetsDir != null ? assetsDir.getPath() : null);
+        addArgument("--version", profile);
+    }
+
+    public void addArgument(String name, String value) {
+        if (value != null) {
+            arguments.add(name);
+            arguments.add(value);
+        } else {
+            LogWrapper.severe("Argument %s is %s", name, null);
+        }
+    }
+
+    @Override
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+        List<String> mixins = new ArrayList<>();
+        File mods = new File(gameDir, "mods");
+        if (!mods.exists()) {
+            mods.mkdir();
+        }
+        try {
+            Files.walkFileTree(mods.toPath(), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.toFile().getName().endsWith(".jar")) {
+                        try {
+                            JarFile jarFile = new JarFile(file.toFile());
+                            if (jarFile.getEntry("ation.mod.json") != null) {
+                                classLoader.addURL(file.toUri().toURL());
+                            } else {
+                                LogWrapper.log("AtionLoader", Level.WARN, "Ignore %s it is not a ation mod or ation.mod.json not found", file.toFile().getName());
+                            }
+                        } catch (Throwable throwable) {
+                            LogWrapper.log("AtionLoader", Level.ERROR, throwable, "File %s fail to load", file.toFile().getName());
+                        }
+                    }
+                    return super.visitFile(file, attrs);
+                }
+            });
+
+            Enumeration<URL> resources = classLoader.getResources("ation.mod.json");
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+
+                ModInfo modInfo = new Gson().fromJson(IOUtils.toString(url.openStream(), StandardCharsets.UTF_8), ModInfo.class);
+                if (modInfo.getMixin() != null) {
+                    mixins.add(modInfo.getMixin());
+                }
+                LogWrapper.log("AtionLoader", Level.INFO, "%s | %s | %s | %s", modInfo.getName(), modInfo.getAuthor(), modInfo.getVersion(), modInfo.getDescription());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        MixinBootstrap.init();
+        mixins.forEach(Mixins::addConfiguration);
+    }
+
+    @Override
+    public String getLaunchTarget() {
+        return "net.minecraft.client.main.Main";
+    }
+
+    @Override
+    public String[] getLaunchArguments() {
+        return arguments.toArray(new String[0]);
+    }
+}
---
+++ src/main/java/io/github/ationmc/loader/mod/ModInfo.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2022 Enaium
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ationmc.loader.mod;
+
+/**
+ * @author Enaium
+ */
+public class ModInfo {
+    private final String name;
+    private final String author;
+    private final String version;
+    private final String description;
+    private final String mixin;
+
+    public ModInfo(String name, String author, String version, String description, String mixin) {
+        this.name = name;
+        this.author = author;
+        this.version = version;
+        this.description = description;
+        this.mixin = mixin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMixin() {
+        return mixin;
+    }
+}
